e45ca8ef37126cf2e66ade0670f8eb1169cd12ab
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-07-17 11:26:04 -0300

    create user route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import { prisma } from './lib/prisma'
+import { z } from 'zod'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
