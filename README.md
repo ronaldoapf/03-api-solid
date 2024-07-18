4d0b11dd1d13f8f0eef2de50800edecee2fa73b2
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-07-18 00:18:40 -0300

    search gyms use case

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Nesse projeto, vamos realizar o desenvolvimento de uma aplicação como o que co
 - [x] Deve ser possível obter o número de check-ins realizados pelo usuário logado;
 - [x] Deve ser possível o usuário obter o seu histórico de check-ins;
 - [ ] Deve ser possível o usuário buscar academias próximas;
-- [ ] Deve ser possível o usuário buscar academias pelo nome;
+- [x] Deve ser possível o usuário buscar academias pelo nome;
 - [x] Deve ser possível o usuário realizar check-in em uma academia;
 - [ ] Deve ser possível validar o check-in de um usuário;
 - [x] Deve ser possível cadastrar uma academia;
---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,62 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { SearchGymsUseCase } from './search-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: SearchGymsUseCase
+
+describe('Search Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new SearchGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to search for gyms', async () => {
+    await gymsRepository.create({
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: -18.9230654,
+      longitude: -48.2939209,
+    })
+
+    await gymsRepository.create({
+      title: 'TypeScript Gym',
+      description: '',
+      phone: '',
+      latitude: -18.9230654,
+      longitude: -48.2939209,
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 1,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JavaScript Gym' })])
+  })
+
+  it('should be able to fetch paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `JavaScript Gym ${i}`,
+        description: '',
+        phone: '',
+        latitude: -18.9230654,
+        longitude: -48.2939209,
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'JavaScript Gym 21' }),
+      expect.objectContaining({ title: 'JavaScript Gym 22' }),
+    ])
+  })
+})
