a3f44dce33ab6044c3143f4278ab177ff6f4ff67
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-07-18 13:16:37 -0300

    use jwt token on fastify

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import { errorHandler } from './error-handler'
+import fastifyJwt from '@fastify/jwt'
+import { env } from './env'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 app.setErrorHandler(errorHandler)
